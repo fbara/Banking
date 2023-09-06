38b486fb708eaafcd7b97cc51e75abaefb3096ab
fbara <fbara1@yahoo.com>
2023-09-06 16:58:15 -0400

    Initial Commit

---
+++ Banking/BankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingApp.swift
+//  Banking
+//
+//  Created by Frank Bara on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
